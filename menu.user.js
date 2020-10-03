dede10cfc08392cbb69903cb7c6ea168e3b9ed11
aniuspms <72159406+aniuspms@users.noreply.github.com>
2020-10-03 14:47:15 +0200

    Update menu.user.js

--- menu.user.js
+++ menu.user.js
@@ -6,8 +6,8 @@
 // @description  FUT21 Autobuyer
 // @author       Rastor
 // @co-author    Tiebe_V
-// @match        https://www.easports.com/uk/fifa/ultimate-team/web-app/*
-// @match        https://www.ea.com/fifa/ultimate-team/web-app/*
+// @match        https://www.ea.com/en-gb/fifa/ultimate-team/web-app/*
+// @match        https://www.ea.com/en-gb/fifa/ultimate-team/web-app/*
 // @grant        none
 // ==/UserScript==
 
